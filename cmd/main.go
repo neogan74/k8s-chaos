c4283a9a956bcdff145edc43225af784fa1e86f9
Gushchin Andrei <neogan74@gmail.com>
2025-10-11 09:26:27 +0500

    add to main delay funcs and helpers

--- cmd/main.go
+++ cmd/main.go
@@ -27,6 +27,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
